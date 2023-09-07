8dea15e55f9f07b9e3ca3131c8af2b6d5067cda7
Ravikant Paudel <developer.paudel@gmail.com>
2023-09-07 17:07:49 +0545

    :sparkles: commented isar db

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -12,10 +12,6 @@ class TaskDataBase {
       "Ravikant",
       "Elza",
     ];
-    // taskList = [
-    //   ["Ravikant", false],
-    //   ["Elza", false],
-    // ];
   }
 
   // load the data from database
@@ -28,3 +24,64 @@ class TaskDataBase {
     _myBox.put("TASK_LIST", offlineList);
   }
 }
+
+
+
+// import 'package:isar/isar.dart';\
+//
+// class DatabaseWrapper {
+//   Isar isar;
+//
+//   DatabaseWrapper({required this.isar});
+//
+//   Future<void> open() async {
+//     await isar.open();
+//   }
+//
+//   Future<void> close() async {
+//     await isar.close();
+//   }
+//
+//   Future<void> insertTask(Task task) async {
+//     await isar.writeTxn((isar) async {
+//       await isar.write<Task>().insert(task);
+//     });
+//   }
+//
+//   Future<void> updateTask(Task task) async {
+//     await isar.writeTxn((isar) async {
+//       await isar.write<Task>().update(task);
+//     });
+//   }
+//
+//   Future<void> deleteTask(int id) async {
+//     await isar.writeTxn((isar) async {
+//       final task = await isar.read<Task>().where().idEqualTo(id).findFirst();
+//       if (task != null) {
+//         await task.delete();
+//       }
+//     });
+//   }
+//
+//   Future<List<Task>> getAllTasks() async {
+//     final tasks = await isar.read<Task>().findAll();
+//     return tasks;
+//   }
+//
+//   Future<List<Task>> getCompletedTasks() async {
+//     final tasks = await isar.read<Task>().where().isCompletedEqualTo(true).findAll();
+//     return tasks;
+//   }
+//
+//   Future<List<Task>> getIncompleteTasks() async {
+//     final tasks = await isar.read<Task>().where().isCompletedEqualTo(false).findAll();
+//     return tasks;
+//   }
+//
+//   Future<void> watchTasks(void Function(List<Task>) callback) async {
+//     final stream = isar.read<Task>().watch();
+//     await for (final event in stream) {
+//       callback(event);
+//     }
+//   }
+// }
